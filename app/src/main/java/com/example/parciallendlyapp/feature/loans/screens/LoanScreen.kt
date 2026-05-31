1066c97c888e4e39de374b291f89903ba312b607
VarasMD <md.varas.arg@gmail.com>
2026-05-31 13:08:26 -0300

    Nuevo componente para loans, InfoContainerProminent.kt

--- app/src/main/java/com/example/parciallendlyapp/feature/loans/screens/LoanScreen.kt
+++ app/src/main/java/com/example/parciallendlyapp/feature/loans/screens/LoanScreen.kt
@@ -1,9 +1,23 @@
 package com.example.parciallendlyapp.feature.loans.screens
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.parciallendlyapp.components.InfoContainerProminent
 
 @Composable
 fun LoanScreen() {
-    Text("LoanScreen")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        InfoContainerProminent(
+            title = "Safe and sound",
+            description = "All here, in one place. Your data is protected."
+        )
+    }
 }
